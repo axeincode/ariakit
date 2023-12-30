axeincode/ariakit

commit cab4e6f9d343d262bc728a84644bc8df15760ba6

Try to sort CollectionItem elements less often (#3241)
Closes #3214

packages/ariakit-core/src/collection/collection-store.ts
@@ -100,34 +100,47 @@ export function createCollectionStore<
 
   const collection = createStore(initialState, props.store);
 
-  const sortItems = () => {
-    const state = privateStore.getState();
-    const renderedItems = sortBasedOnDOMPosition(state.renderedItems);
-    privateStore.setState("renderedItems", renderedItems);
-    collection.setState("renderedItems", renderedItems);
-  };
-
   setup(collection, () => init(privateStore));
 
   setup(privateStore, () => {
     return batch(privateStore, ["renderedItems"], (state) => {
       let firstRun = true;
-      let raf = requestAnimationFrame(sortItems);
-      if (typeof IntersectionObserver !== "function") return;
-      const callback = () => {
+
+      const raf = requestAnimationFrame(() => {
+        const { renderedItems } = collection.getState();
+        // Bail out if the rendered items haven't changed. This is important
+        // because the following lines will cause this function to be called
+        // again.
+        if (state.renderedItems === renderedItems) return;
+        const sortedItems = sortBasedOnDOMPosition(state.renderedItems);
+        privateStore.setState("renderedItems", sortedItems);
+        collection.setState("renderedItems", sortedItems);
+      });
+
+      if (typeof IntersectionObserver !== "function") {
+        return () => cancelAnimationFrame(raf);
+      }
+
+      const ioCallback = () => {
         if (firstRun) {
+          // The IntersectionObserver callback is called synchronously the first
+          // time. We just ignore it.
           firstRun = false;
           return;
         }
-        cancelAnimationFrame(raf);
-        raf = requestAnimationFrame(sortItems);
+        // Re-trigger the the private store callback to sort the items again in
+        // the requestAnimationFrame callback above.
+        privateStore.setState("renderedItems", [...state.renderedItems]);
       };
+
       const root = getCommonParent(state.renderedItems);
-      const observer = new IntersectionObserver(callback, { root });
-      state.renderedItems.forEach((item) => {
-        if (!item.element) return;
+      const observer = new IntersectionObserver(ioCallback, { root });
+
+      for (const item of state.renderedItems) {
+        if (!item.element) continue;
         observer.observe(item.element);
-      });
+      }
+
       return () => {
         cancelAnimationFrame(raf);
         observer.disconnect();

commit 2edbae09905f4ad4830f3038023d7f5d7d88b9b9

Fix regression when scrolling composite items (#3286)
Fix a regression introduced by #3241 where the `renderedItems` state was
being reset on every scroll event.

Closes #3285
Re-opens #3214

examples/menu-maximum-update-depth/index.tsx
@@ -1,0 +1,19 @@
+import "./style.css";
+import * as Ariakit from "@ariakit/react";
+
+function Menu() {
+  return (
+    <Ariakit.MenuProvider>
+      <Ariakit.MenuButton className="button">Menu</Ariakit.MenuButton>
+      <Ariakit.Menu className="menu">
+        <Ariakit.MenuItem className="menu-item">A</Ariakit.MenuItem>
+        <Ariakit.MenuItem className="menu-item">B</Ariakit.MenuItem>
+        <Ariakit.MenuItem className="menu-item">C</Ariakit.MenuItem>
+      </Ariakit.Menu>
+    </Ariakit.MenuProvider>
+  );
+}
+
+export default function Example() {
+  return Array.from({ length: 60 }, (_, index) => <Menu key={index} />);
+}

examples/menu-maximum-update-depth/style.css
@@ -1,0 +1,1 @@
+@import url("../menu/style.css");

packages/ariakit-core/src/collection/collection-store.ts
@@ -100,37 +100,40 @@ export function createCollectionStore<
 
   const collection = createStore(initialState, props.store);
 
+  const sortItems = (renderedItems: T[]) => {
+    const sortedItems = sortBasedOnDOMPosition(renderedItems);
+    privateStore.setState("renderedItems", sortedItems);
+    collection.setState("renderedItems", sortedItems);
+  };
+
   setup(collection, () => init(privateStore));
 
   setup(privateStore, () => {
     return batch(privateStore, ["renderedItems"], (state) => {
       let firstRun = true;
 
-      const raf = requestAnimationFrame(() => {
+      let raf = requestAnimationFrame(() => {
         const { renderedItems } = collection.getState();
         // Bail out if the rendered items haven't changed. This is important
-        // because the following lines will cause this function to be called
+        // because the following lines can cause this function to be called
         // again.
         if (state.renderedItems === renderedItems) return;
-        const sortedItems = sortBasedOnDOMPosition(state.renderedItems);
-        privateStore.setState("renderedItems", sortedItems);
-        collection.setState("renderedItems", sortedItems);
+        sortItems(state.renderedItems);
       });
 
       if (typeof IntersectionObserver !== "function") {
         return () => cancelAnimationFrame(raf);
       }
 
-      const ioCallback = () => {
+      const ioCallback: IntersectionObserverCallback = () => {
         if (firstRun) {
           // The IntersectionObserver callback is called synchronously the first
           // time. We just ignore it.
           firstRun = false;
           return;
         }
-        // Re-trigger the the private store callback to sort the items again in
-        // the requestAnimationFrame callback above.
-        privateStore.setState("renderedItems", [...state.renderedItems]);
+        cancelAnimationFrame(raf);
+        raf = requestAnimationFrame(() => sortItems(state.renderedItems));
       };
 
       const root = getCommonParent(state.renderedItems);

packages/ariakit-core/src/utils/store.ts
@@ -78,7 +78,6 @@ export function createStore<S extends State>(
   };
 
   const storeInit: StoreInit = () => {
-    if (!stores.length) return noop;
     // Make sure we only initialize the store once, even when it's passed to
     // other stores. However, the store can't be destroyed until all instances
     // are unmounted. See https://github.com/ariakit/ariakit/issues/3147. See

examples/combobox-multiple/test-browser.ts
@@ -28,3 +28,11 @@ test("scroll offscreen item into view after selecting it", async ({ page }) => {
   await expect(q.option("Pineapple")).toHaveAttribute("data-active-item", "");
   await expect(q.option("Pineapple")).toHaveAttribute("aria-selected", "true");
 });
+
+test("scroll after hovering over an item", async ({ page }) => {
+  const q = query(page);
+  await q.combobox().click();
+  await q.option("Apple").hover();
+  await page.mouse.wheel(0, 200);
+  await expect(q.option("Apple")).not.toBeInViewport();
+});